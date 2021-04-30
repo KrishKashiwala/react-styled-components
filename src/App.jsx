ab77da45fa0d3976ebf1c84884c774dcb8ae483f
KrishKashiwala <krishkashiwala@gmail.com>
2021-04-30 18:55:40 +0530

    no-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
 import './App.css';
 import styled from 'styled-components'
+
+
 const Button = styled.button`
-       cursor: pointer;
+  cursor: pointer;
   background: transparent;
   font-size: 16px;
   border-radius: 3px;
-  color: ${props => props.primary ? 'red' : 'palevioletred'} ;
+  color: ${props => props.danger? 'rgb(200,200,200)' : 'black'} ;
   border: 2px solid palevioletred;
   margin: 0 1em;
   padding: 0.25em 1em;
   transition: 0.5s all ease-out;
+  display : flex;
+  justify-content: flex-end;
  
   &:hover {
     background-color: palevioletred;
     color: white;
   }
 `
-const Upbutton  = styled(Button)`
-      border : 5px dashed red;
-`
-
 const App = () =>{
       return (
             <>
-                        
-                        <Button primary> Click Me and see changes!</Button>
+                        <Button warning danger> Click Me and see changes!</Button>
             </>
       )
       };
